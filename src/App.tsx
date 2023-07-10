60fea39345440e65bed1e1bab0ee78a1b13fed17
Pappu Dutta <pappudutta279@gmail.com>
2023-07-10 17:08:25 +0530

    feat: Fetch games

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 import "./App.css";
 import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
@@ -20,7 +21,9 @@ function App() {
         <GridItem area="aside">aside</GridItem>
       </Show>
 
-      <GridItem area="main">Main</GridItem>
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
     </Grid>
   );
 }
