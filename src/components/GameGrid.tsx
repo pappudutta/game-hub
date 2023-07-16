1063353c0325b86812d5748a5f40ab6b2247c13c
Pappu Dutta <pappudutta279@gmail.com>
2023-07-16 13:53:26 +0530

    feat: build game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,22 @@
-import { Text } from "@chakra-ui/react";
-import UseGames from "../hooks/UseGames";
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import UseGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error } = UseGames();
   return (
     <>
       {error && <Text>{error}</Text>}
-      <ul>
+      <SimpleGrid
+        column={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding={5}
+        spacing={10}
+        templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+      >
         {games.map(game => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
