60b7aee0836823cab7f150ae5932c4a971111bf5
Pappu Dutta <pappudutta279@gmail.com>
2023-07-19 16:03:30 +0530

    Navbar Rebuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <h1>React18 app</h1>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"red.400"} padding={2}>
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"green.300"}>
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"yellow.300"}>
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
