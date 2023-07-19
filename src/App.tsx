31f40cab3e394871c274b5846b1302d55b374d09
Pappu Dutta <pappudutta279@gmail.com>
2023-07-19 13:26:13 +0530

    Reset For Recreation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,5 @@
-import { useState } from "react";
-import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
-
-import "./App.css";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-
 function App() {
-  return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-
-      <Show above="lg">
-        <GridItem area="aside">
-          <GenreList />
-        </GridItem>
-      </Show>
-
-      <GridItem area="main">
-        <GameGrid />
-      </GridItem>
-    </Grid>
-  );
+  return <h1>React18 app</h1>;
 }
 
 export default App;
--- src/services/api-client.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '0db02b49e417479e8ee2aa21c34a244a'
-    }
-})
